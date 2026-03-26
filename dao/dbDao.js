a83609c400ef6ba00cb3deba48a98366d8891f4b
Sergey Eskov <yeskovsj@gmail.com>
2026-03-26 20:43:46 +0300

    Refactoring: clusterIdHolder as parameter

---
+++ dao/dbDao.js
@@ -0,0 +1,12 @@
+class DBDao {
+    constructor () {
+        // TODO: connect db
+    }
+
+    // TODO: implement processing transactions
+    processTransactions (onProcessTransaction) {
+
+    }
+}
+
+module.exports = DBDao;
